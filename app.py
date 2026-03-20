28ece260ed0fbd691122c82dc2f6371c6fd7afb4
shrikantdeshmukh9766-ux <shrikantdeshmukh9766@gmail.com>
2026-03-20 23:19:13 +0530

    app.py

--- app.py
+++ app.py
@@ -299,7 +299,7 @@ hr {
 # =====================
 TOKEN     = "23801d339dd6d16509a79250731f126401d5f7a3"
 BASE_URL  = "https://kobo.humanitarianresponse.info/api/v2"
-asset_uid = "afWux6DQFqmZrEpK54BobD"
+asset_uid = "auv4fwhA4rcKi5eFPU6XQj"
 
 # =====================
 # LOAD DATA
